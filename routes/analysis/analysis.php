3ced5b3be95313044d38a869834f7c8e7ab8df1a
Obi Pascal <pascalobi83@gmail.com>
2023-08-17 09:00:39 +0100

    updated

--- routes/analysis/analysis.php
+++ routes/analysis/analysis.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Analysis\AnalysisApis;
 use Illuminate\Support\Facades\Route;
 
-Route::group(["prefix" => "v1", "controller" => AnalysisApis::class, "middleware" => ["auth:sanctum", "adminOnly"]], function () {
+Route::group(["prefix" => "v1", "controller" => AnalysisApis::class], function () {
 	/**
 	 * @todo Analysis rest
 	 * @api /api/v1/analysis
